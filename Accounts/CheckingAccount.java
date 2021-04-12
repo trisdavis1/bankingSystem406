fb293bb2e258f229cd9e8067079abd93d27cb180
reaperjudge <38620064+reaperjudge@users.noreply.github.com>
2021-04-12 15:01:34 -0500

    tried to help with checking account

--- Accounts/CheckingAccount.java
+++ Accounts/CheckingAccount.java
@@ -1,4 +1,24 @@
 package Accounts;
 
 public class CheckingAccount extends Account {
+    public int customerId;
+    public int accountNumber;
+    public String accountType;
+    public double currentBalance;
+    public int backupAccount;
+    public String backNumber;
+    public int overdrafts;
+    public String dateOpened;
+
+    public double interestRate;
+
+    public CheckingAccount(int customerId,int accountNumber,
+        String accountType, double currentBalance,
+        int backupAccount, String backNumber,
+        int overDrafts, String dateOpened){
+
+        this.customerId = customerId;
+        this.currentBalance = currentBalance;
+        this.interestRate=interestRate;
+    }
 }
--- main.java
+++ main.java
@@ -1,4 +1,5 @@
 import Accounts.Account;
+import Accounts.CheckingAccount;
 import Accounts.LoanAccount;
 import Menus.Menu;
 import Menus.initATM;
@@ -12,7 +13,9 @@ import java.util.List;
 public class main {
     public static void main(String[] args) throws IOException {
         File loanFile = new File("../bankingSystem406/Database Files/Loans.txt");
+        File checkFile = new File("../bankingSystem406/Database Files/Checking_Accounts.txt");
         List<LoanAccount> loanAccounts = new ArrayList<>();
+        List<CheckingAccount> checkAccounts = new ArrayList<>();
         List<Account> accountList = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(loanFile))) {
@@ -39,5 +42,32 @@ public class main {
         for(var loan : loanAccounts){
             System.out.println(loan.currentBalance); // just to verify its working (can be deleted)
         }
+        /* try (BufferedReader br = new BufferedReader(new FileReader(checkFile))) {
+            
+            String line;
+            while((line = br.readLine()) != null){
+
+                String[] items = line.split(",");
+
+                checkAccounts.add(new CheckingAccount(
+                    Integer.parseInt(items[0]),
+                    Integer.parseInt(items[1]),
+                    items[2],Double.parseDouble(items[3]), Integer.parseInt(items[4]),
+                    items[5], Integer.parseInt(items[6]), items[7])
+                );
+            }
+            //open main Menu
+        } catch (Exception e) {
+            e.printStackTrace();
+        } */
+        Menu menu= new Menu();
+        menu.openMenu(accountList);
+        for(var loan : loanAccounts){
+           // System.out.println(loan.currentBalance); // just to verify its working (can be deleted)
+        }
+        for(var account : checkAccounts){
+            System.out.println(account.currentBalance); // just to verify its working (can be deleted)
+        }
+
     }
 }
